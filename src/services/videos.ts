588e90e3f248c64b83abd12bb6652a000ce116d4
Birk Brauer <3842623+bb1@users.noreply.github.com>
2021-02-19 19:40:22 +0100

    finsihed array merging ✨

--- src/services/videos.ts
+++ src/services/videos.ts
@@ -7,9 +7,19 @@ export const getVideos = async (abortSignal: AbortSignal): Promise<ProcessedVide
 
   const [categories, authors] = await Promise.all([
     getCategories(abortSignal),
-    getAuthors(abortSignal)
+    getAuthors(abortSignal),
   ]);
 
-  return [];
+  const moisteredAuthors = authors.reduce((acc, {name: author, videos}) => {
+    const curVideos: ProcessedVideo[] = videos.map(({id, name, catIds}) => ({
+      id,
+      name,
+      author,
+      categories: catIds.map(catId => categories.find(({id}) => id === catId)?.name || ''),
+    }));
 
+    return [...acc, ...curVideos];
+  }, <ProcessedVideo[]>[]);
+
+  return moisteredAuthors;
 };
