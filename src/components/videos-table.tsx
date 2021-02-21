6b2e025483e81c93d0416e941688c259fbdd035e
Birk Brauer <3842623+bb1@users.noreply.github.com>
2021-02-21 01:43:42 +0100

    color highlight for quality 🏆

--- src/components/videos-table.tsx
+++ src/components/videos-table.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Chip, Avatar } from '@material-ui/core';
 import { ProcessedVideo } from '../common/interfaces';
+
+import styles from './videos-table.module.scss';
 
 interface VideosTableProps {
     videos: ProcessedVideo[];
@@ -28,7 +30,13 @@ export const VideosTable: React.FC<VideosTableProps> = ({ videos }) => {
               </TableCell>
               <TableCell>{video.author}</TableCell>
               <TableCell>{video.categories.join(', ')}</TableCell>
-              <TableCell>{video.formatName} {video.res}</TableCell>
+              <TableCell>
+                <Chip
+                  label={video.formatName}
+                  avatar={<Avatar className={styles.resolution}>{video.res}</Avatar>}
+                  className={video.formatName && styles[video.formatName]}
+                />
+              </TableCell>
               <TableCell>{video.releaseDate}</TableCell>
               <TableCell> {/* add buttons here as needed */}  </TableCell>
             </TableRow>
